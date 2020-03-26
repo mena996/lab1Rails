1bb5070071eb07941d5c415d79faf78e7a7a9ba1
mena996 <menaemad996@gmail.com>
2020-03-26 23:12:39 +0200

    adding lab2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates_uniqueness_of :title
+    validates :title, length: { maximum: 50 }
+    validates :title, :content, presence: true
+    has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'comments/new(/:id)', to: 'comments#new', as: :new_comment
+  get '(/:id)/comments', to: 'comments#index', as: :comments
+  get '/comment/(/:id)/edit', to: 'comments#edit', as: :edit_comment
+  delete '/:id/comment', to: 'comments#destroy', as: :delete_comment
+  post '(/:id)/comment', to: 'comments#create', as: :comment
+  patch '/comment/:id', controller: 'comments', action: :update ,as: :update_comment
+
   resources :posts
   root to: 'posts#index'
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
